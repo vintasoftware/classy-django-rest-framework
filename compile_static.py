d50f1466ff6f87dd93dbafce227d972802d3073e
André Ericson <de.ericson@gmail.com>
2015-03-03 15:28:38 -0300

    Add index.html and move generated files to public

--- compile_static.py
+++ compile_static.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
 import os, errno
+import jinja2
 
 from rest_framework_ccbv.inspector import drfviews
 from rest_framework_ccbv.page_generator import Generator
+
+templateLoader = jinja2.FileSystemLoader(searchpath="templates")
+templateEnv = jinja2.Environment(loader=templateLoader)
 
 
 def mkdir_p(path):
@@ -15,12 +19,16 @@ def mkdir_p(path):
         else:
             raise
 
+
 def main():
     for view in drfviews.values():
         generator = Generator(view.__name__, view.__module__)
-        mkdir_p(os.path.join('static', view.__module__))
-        generator.generate(filename=os.path.join('static', view.__module__,
+        mkdir_p(os.path.join('public', view.__module__))
+        generator.generate(filename=os.path.join('public', view.__module__,
                                                  view.__name__ + '.html'))
+    template = templateEnv.get_template('list.html')
+    with open(os.path.join('public', 'index.html'), 'w') as f:
+        f.write(template.render({'views': drfviews.values()}))
 
 
 if __name__ == '__main__':
