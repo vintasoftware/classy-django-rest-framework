66b54b0bf9de6f14131db449cf6b71f9f1c50d0f
André Ericson <de.ericson@gmail.com>
2016-07-19 20:03:28 -0300

    Sitemap test

--- tests/test_renderers.py
+++ tests/test_renderers.py
@@ -4,8 +4,10 @@ from mock import mock_open, patch
 from rest_framework.generics import ListAPIView
 
 from rest_framework_ccbv.renderers import (
-    BasePageRenderer, IndexPageRenderer, LandPageRenderer, ErrorPageRenderer
+    BasePageRenderer, IndexPageRenderer, LandPageRenderer, ErrorPageRenderer,
+    SitemapRenderer,
 )
+
 
 class TestBasePageRenderer(unittest.TestCase):
     def setUp(self):
@@ -50,3 +52,17 @@ class TestStaticPagesRenderered(unittest.TestCase):
         get_template_mock.assert_called_with('home.html')
         self.rendererErrorPage.render('foo')
         get_template_mock.assert_called_with('error.html')
+
+
+class TestSitemapRenderer(unittest.TestCase):
+    def setUp(self):
+        self.renderer = SitemapRenderer([ListAPIView])
+
+    @patch('rest_framework_ccbv.renderers.templateEnv.get_template')
+    @patch('rest_framework_ccbv.renderers.open', mock_open(read_data='{}'))
+    def test_context(self, get_template_mock):
+        self.renderer.render('foo')
+        context = get_template_mock.return_value.render.call_args_list[0][0][0]
+        assert context['latest_version']
+        assert context['base_url']
+        assert context['klasses'] == {}
