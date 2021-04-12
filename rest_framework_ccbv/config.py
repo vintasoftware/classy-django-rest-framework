d0155ff96e28e2496a217636742f57e9d20639d4
rvlb-19 <renatovlbarros19@gmail.com>
2021-04-12 17:07:13 -0300

    Bring older versions back

--- rest_framework_ccbv/config.py
+++ rest_framework_ccbv/config.py
@@ -2,6 +2,17 @@ from rest_framework import VERSION as rest_framework_version
 
 
 REST_FRAMEWORK_VERSIONS = [
+    '3.1',
+    '3.2',
+    '3.3',
+    '3.4',
+    '3.5',
+    '3.6',
+    '3.7',
+    '3.8',
+    '3.9',
+    '3.10',
+    '3.11',
     '3.12',
 ]
 
